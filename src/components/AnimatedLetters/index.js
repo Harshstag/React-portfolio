a7fba7da555d65a1f338c2550e43293158b7dca8
Harsh Singh <hrsharshsingh@gmail.com>
2023-08-07 20:37:43 +0530

    first commit

---
+++ src/components/AnimatedLetters/index.js
@@ -0,0 +1,15 @@
+import './index.scss'
+
+const Animatedletters = ({ letterClass, strArray, idx }) => {
+  return (
+    <span>
+      {strArray.map((char, i) => (
+        <span key={char + i} className={`${letterClass} _${i + idx}`}>
+          {char}
+        </span>
+      ))}
+    </span>
+  )
+}
+
+export default Animatedletters
